676e4320dd63acb03b56e97c066a23937f2e7082
jssprz <jperezmartin90@gmail.com>
2021-04-07 13:15:58 -0400

    changes the get_freer_gpu fuction

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ __status__ = "Development"
 def get_freer_gpu():
     os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
     memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
-    return np.argsort(memory_available)[::-1]
+    return np.argmax(memory_available)
 
 
 # def get_tags_feat(captions, tags_vocab):
